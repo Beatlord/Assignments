34d8bee2e5c37152d78202b76d78ceca651e1020
Ebenezer <164800458+Beatlord@users.noreply.github.com>
2024-10-24 13:45:41 +0300

    Add files via upload

--- question 4.py
+++ question 4.py
@@ -1,7 +1,8 @@
 def func(numb):
-    sum =0
-    print('The perfect numbers below {numb} are:')
+    num =1
+    print(f'The perfect numbers below {numb} are:')
     for num in range(1, numb):
+        sum =0
         for n in range(1, num):
             if num %n ==0:
                 sum += n
