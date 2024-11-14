2e915de209912f1c988c229050e4dcaa0bf36044
Ebenezer <164800458+Beatlord@users.noreply.github.com>
2024-11-14 09:18:47 +0200

    Update question3.py

--- question3.py
+++ question3.py
@@ -2,7 +2,7 @@ def vow_omit():#defining the function to omit vowels
     user_str = input('Enter your string: ')#prompting for user input
     f_str =" "
     for str in user_str:#checking for vowels
-        if str =='a' or str =='e' or str =='i' or str =='o' or str =='u' or str =='A' or str =='E' or str =='I' or str == 'O' or str =='U':
+        if str in 'aeiouAEIOU':
             continue
         else:
             f_str = f_str + str #final string initialisation
